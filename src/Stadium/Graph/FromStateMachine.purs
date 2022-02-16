472b97117448fa2731f0c9447003d348b7cede13
Michael Bock <mb@thought2.de>
2022-02-16 17:59:14 +0100

    Finish graph generation

--- src/Stadium/Graph/FromStateMachine.purs
+++ src/Stadium/Graph/FromStateMachine.purs
@@ -1,7 +1,7 @@
 module Stadium.Graph.FromStateMachine where
 
 import Prelude
-import Data.Array (fold)
+import Data.Array (fold, (:))
 import Data.Tuple.Nested (type (/\), (/\))
 import Stadium.Graph.Type as G
 import Stadium.Reflect as R
@@ -23,27 +23,28 @@ fromStateMachineData n sd =
 
 findStateNode :: R.ActionName /\ R.StateData -> Graph'
 findStateNode (name /\ sd) =
-  fold
-    ( [ { nodes: [ G.State { name } ]
+  map (findActionName name) sd
+    # (:)
+        { nodes: [ G.State { name } ]
         , edges: []
         }
-      ]
-        <> map (findActionName name) sd
-    )
+    # fold
 
 findActionName :: R.StateName -> (R.ActionName /\ R.ActionData) -> Graph'
-findActionName sn (an /\ _) =
-  { nodes:
-      [ G.Action
-          { name: actionName
-          }
-      ]
-  , edges:
-      [ G.ToAction { fromState: sn, toAction: actionName }
-      ]
-  }
+findActionName sn (an /\ ad) =
+  map (findToState sn an) ad
+    # (:)
+        { nodes: [ G.Action { name: actionName } ]
+        , edges: [ G.ToAction { fromState: sn, toAction: actionName } ]
+        }
+    # fold
   where
   actionName = { state: sn, action: an }
 
 findToState :: R.StateName -> R.ActionName -> R.StateName -> Graph'
-findToState sn an ts = undefined
+findToState sn an ts =
+  { nodes: []
+  , edges: [ G.ToState { fromAction: actionName, toState: ts } ]
+  }
+  where
+  actionName = { state: sn, action: an }
