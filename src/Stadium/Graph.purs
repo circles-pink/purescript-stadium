42acbef6e7a42a075d54866184a623fc7247bc40
nmauersberg <n.mauersberg@gmail.com>
2022-02-16 13:19:03 +0100

    Implement Dot and graphToDot

--- src/Stadium/Graph.purs
+++ src/Stadium/Graph.purs
@@ -1,22 +1,23 @@
 module Stadium.Graph where
 
-import Prelude
-
 type StateName
   = String
 
 type ActionName
-  = String
+  = { state :: StateName
+    , action :: String
+    }
 
 data Node
-  = Action { state :: StateName, action :: ActionName }
-  | State { state :: StateName }
+  = Action { name :: ActionName }
+  | State { name :: StateName }
 
 data Edge
   = ToState { fromAction :: ActionName, toState :: StateName }
   | ToAction { fromState :: StateName, toAction :: ActionName }
 
 type Graph
-  = { nodes :: Array Node
+  = { name :: String
+    , nodes :: Array Node
     , edges :: Array Edge
     }
--- src/Stadium/Graph/Dot.purs
+++ src/Stadium/Graph/Dot.purs
@@ -1,5 +1,39 @@
-module Stadium.Graph.Dot where
+module Stadium.Graph.Dot
+  ( graphToDot
+  ) where
 
 import Prelude
+import Dot as D
+import Stadium.Graph as G
 
---import Data.DotLang
+graphToDot :: G.Graph -> D.Dot
+graphToDot g =
+  { name: g.name
+  , nodes: map fromNode g.nodes
+  , edges: map fromEdge g.edges
+  }
+
+fromNode :: G.Node -> D.Node
+fromNode n = case n of
+  G.State s ->
+    { id: s.name
+    , shape: D.Ellipse
+    }
+  G.Action s ->
+    { id: fromActionName s.name
+    , shape: D.Ellipse
+    }
+
+fromEdge :: G.Edge -> D.Edge
+fromEdge n = case n of
+  G.ToState s ->
+    { from: fromActionName s.fromAction
+    , to: s.toState
+    }
+  G.ToAction s ->
+    { from: s.fromState
+    , to: fromActionName s.toAction
+    }
+
+fromActionName :: G.ActionName -> String
+fromActionName a = a.state <> "__" <> a.action
