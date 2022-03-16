12c32f7289b5a3544d9dfbac41d5b610344a6d24
Michael Bock <me@thought2.de>
2022-03-16 16:30:31 +0100

    Use dot-language lib

--- src/Stadium/Graph/ToDot.purs
+++ src/Stadium/Graph/ToDot.purs
@@ -3,65 +3,68 @@ module Stadium.Graph.ToDot
   ) where
 
 import Prelude
-
 import Data.Array as A
 import Data.Maybe (Maybe(..))
-import Stadium.Graph.Type (Node(..))
 import Language.Dot as D
+import Stadium.Graph.Type (Node(..))
 import Stadium.Graph.Type as G
-import Undefined (undefined)
 
-type Options = {
-  entryPoint:: Maybe String 
-}
+type Options
+  = { entryPoint :: Maybe String
+    }
 
 graphToDot :: Options -> G.Graph -> D.Graph
-graphToDot = undefined
+graphToDot opt g =
+  D.Graph
+    { strict: true
+    , type: D.directed_
+    , id: Just $ D.Id g.name
+    , stmts:
+        (g.nodes # reorder' # map (fromNode >>> D.nodeStmt))
+          <> (map (fromEdge >>> D.edgeStmt) g.edges)
+    }
+  where
+  reorder' = case opt.entryPoint of
+    Nothing -> identity
+    Just ep -> reorder ep
 
--- graphToDot :: Options -> G.Graph -> D.Dot
--- graphToDot opt g =
---   { name: g.name
---   , nodes: g.nodes # reorder' # map fromNode 
---   , edges: map fromEdge g.edges
---   }
---   where
---   reorder' = case opt.entryPoint of
---     Nothing -> identity
---     Just ep -> reorder ep 
-  
--- reorder :: String -> Array Node -> Array Node
--- reorder entryName xs = r.rest <> r.init
---   where
---   r = A.span (\n -> nodeToStateName n /= Just entryName) xs
+fromNode :: G.Node -> D.NodeStmt
+fromNode n = case n of
+  G.State s ->
+    D.NodeStmt
+      { id: D.NodeId { id: D.Id s.name }
+      , attrs: [ D.label s.name ]
+      }
+  G.Action s ->
+    D.NodeStmt
+      { id: D.NodeId { id: D.Id $ fromActionName s.name }
+      , attrs: [ D.label s.name.action ]
+      }
 
---   nodeToStateName :: Node -> Maybe String
---   nodeToStateName n = case n of
---     State {name} -> Just name 
---     _ -> Nothing 
-  
--- fromNode :: G.Node -> D.Node
--- fromNode n = case n of
---   G.State s ->
---     { id: s.name
---     , shape: D.Ellipse
---     , label: s.name
---     }
---   G.Action s ->
---     { id: fromActionName s.name
---     , shape: D.Box
---     , label: s.name.action
---     }
+fromEdge :: G.Edge -> D.EdgeStmt
+fromEdge n = case n of
+  G.ToState s ->
+    D.EdgeStmt
+      (D.nodeId $ D.NodeId { id: D.Id $ fromActionName s.fromAction })
+      (D.nodeId $ D.NodeId { id: D.Id s.toState })
+      []
+      []
+  G.ToAction s ->
+    D.EdgeStmt
+      (D.nodeId $ D.NodeId { id: D.Id s.fromState })
+      (D.nodeId $ D.NodeId { id: D.Id $ fromActionName s.toAction })
+      []
+      []
 
--- fromEdge :: G.Edge -> D.Edge
--- fromEdge n = case n of
---   G.ToState s ->
---     { from: fromActionName s.fromAction
---     , to: s.toState
---     }
---   G.ToAction s ->
---     { from: s.fromState
---     , to: fromActionName s.toAction
---     }
+fromActionName :: G.ActionName -> String
+fromActionName a = a.state <> "__" <> a.action
 
--- fromActionName :: G.ActionName -> String
--- fromActionName a = a.state <> "__" <> a.action
+reorder :: String -> Array Node -> Array Node
+reorder entryName xs = r.rest <> r.init
+  where
+  r = A.span (\n -> nodeToStateName n /= Just entryName) xs
+
+  nodeToStateName :: Node -> Maybe String
+  nodeToStateName n = case n of
+    State { name } -> Just name
+    _ -> Nothing
