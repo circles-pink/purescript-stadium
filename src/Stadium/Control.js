cc7652b096bb93d13c4b8f5755e89ce5e99d2430
Michael Bock <mb@thought2.de>
2022-06-09 12:16:05 +0200

    Fix unnecessary control trigger

--- src/Stadium/Control.js
+++ src/Stadium/Control.js
@@ -1,41 +1,38 @@
 exports.mkControlImpl = function (spec) {
-    return function (setState) {
-      return function (st) {
-        return function (ac) {
-          var setState_ = function (f) {
-            var f_ = function (x) {
-              if (x.type === st.type) {
-                return f(x.value);
-              } else {
-                console.log(
-                  `WARNING: Dropping state update: Action ${ac.value.type} triggered from state ${st.type} but received in state ${x.type} with the following data: `,
-                  ac.value.value
-                );
-                return x;
-              }
-            };
-  
-            return setState(f_);
+  return function (setState) {
+    return function (st) {
+      return function (ac) {
+        var setState_ = function (f) {
+          var f_ = function (x) {
+            if (x.type === st.type) {
+              return f(x.value);
+            } else {
+              console.log(
+                `WARNING: Dropping state update: Action ${ac.value.type} triggered from state ${st.type} but received in state ${x.type} with the following data: `,
+                ac.value.value
+              );
+              return x;
+            }
           };
-  
-          var setStateNoOp = function (f) {
-            return setState((x) => x);
-          };
-  
-          if (ac.type !== st.type) {
-            console.log(
-              `WARNING: Action ${ac.type}/${ac.value.type} was triggered from state ${st.type}`
-            );
-            return spec[ac.type][ac.value.type](setStateNoOp)(st.value)(
-              ac.value.value
-            );
-          }
-  
-          return spec[ac.type][ac.value.type](setState_)(st.value)(
-            ac.value.value
-          );
+
+          return setState(f_);
         };
+
+        var setStateNoOp = function (f) {
+          return setState((x) => x);
+        };
+
+        if (ac.type !== st.type) {
+          console.log(
+            `WARNING: Action ${ac.type}/${ac.value.type} was triggered from state ${st.type}`
+          );
+          return st.value;
+        }
+
+        return spec[ac.type][ac.value.type](setState_)(st.value)(
+          ac.value.value
+        );
       };
     };
   };
-  
+};
